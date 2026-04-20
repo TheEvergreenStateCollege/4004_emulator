5f43c47e339ca60477461cf7f731c214f04353c4
Andersen Ader <andersen.ader@evergreen.edu>
2026-04-19 20:15:20 -0700

    Added function prototypes for bitwise memory operations

--- main/main.c
+++ main/main.c
@@ -9,18 +9,13 @@
  * Functions for instructions
  * Add remaining registers
 */
+
 typedef unsigned _BitInt(4) uint4_t;
 typedef struct i4004_flags {
 	 uint8_t chip_select;
 } i4004_flags;
 typedef struct i4004_registers {
-	union {
-		struct {
-			unsigned char r0;
-			unsigned char r1;
-		};
-		unsigned short r01;
-	};
+	uint8_t r01, r23, r45, r67, r89, r1011, r1213, r1415;
 } i4004_registers;
 
 i4004_flags flags;
@@ -28,6 +23,11 @@ i4004_registers registers;
 uint8_t i4001[256];
 uint4_t i4002[20];
 
+//Memory operation function prototypes
+void insertInRegister(uint4_t value, uint8_t reg);
+uint4_t fetchFromRegister(uint8_t reg);
+
+//Instruction function prototypes
 void NOP(void);
 
 int main(int argc, char *argv[]) {
@@ -36,23 +36,50 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	
-        //open file
+	//open file
 	FILE *file = fopen(argv[1], "r");
 	if(!file) {
 		printf("%s is not a valid file.\n", argv[1]);
 		exit(1);
 	}
-
+	
 	uint8_t WORD;
 	int i = 0;
-	while((WORD = (uint8_t) fgetc(file)) != EOF) { 
-		i4001[i] = WORD;
+	while(i < 256) { 
+		i4001[i] = 0;
+		//i4001[i] = WORD;
+		// (WORD = (uint8_t) fgetc(file)) 
 		++i;
 	}
 	
 	//Free memory and close file
-	free(file);
+	fclose(file);
 	return 0;
+}
+
+void insertInRegister(uint8_t value, uint8_t reg) {
+	switch (reg) {
+		case 0:
+			
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		default:
+	}
+
+	return;
 }
 
 void NOP(void) {
